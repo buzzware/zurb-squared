00783d5628cee5d6f4615f7806013a09b907b80f
Squarespace, Inc. <noreply@squarespace.com>
2012-09-23 21:44:03 +0000

    assets/readme.md blocks/navigation.block collections/blog.conf collections/blog.item collections/blog.list collections/folders.conf pages/readme.md scripts/site.js site.region styles/base.less styles/reset.css template.conf

---
+++ scripts/site.js
@@ -0,0 +1,13 @@
+
+Y.use( 'node', function( Y ) {
+
+  Y.on( 'domready', function() {
+
+    // Init ImageLoader  
+    new Y.Squarespace.Loader({
+      img:Y.all( 'img[data-image]' )
+    });       
+    
+  });
+
+});
